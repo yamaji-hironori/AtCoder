81992b6fe0440727f861e408ede7a5203cac04d6
yamaji-hironori <abc.3517.efg@gmail.com>
2022-03-31 00:56:49 +0900

    Add: 245 f

--- ABC/245/f.cpp
+++ ABC/245/f.cpp
@@ -19,9 +19,23 @@ struct WalkRoute {
     ll from, to;
 };
 struct VertexStatus {
-    int status = -1; // -1: 未計算, 0: 移動不可, 1: 移動可
-    vll canGo; // 頂点から移動できる頂点群
+    int status = 0; // -1: 移動不可, 0: 未計算, 1: 移動可
+    int aliveToRouteNum = 0;
+    vll toRoute; // この頂点から行くことが出来る頂点群
+    vll fromRoute; // この頂点へ行くことが出来る頂点群
 };
+
+void routeBack(vector<VertexStatus> &vertices, ll i_vertex_num) {
+    if (vertices.at(i_vertex_num).aliveToRouteNum != 0) {
+        return;
+    }
+    vertices.at(i_vertex_num).status = -1;
+    for (ll i_fromRoute: vertices.at(i_vertex_num).fromRoute) {
+        vertices.at(i_fromRoute).aliveToRouteNum--;
+        routeBack(vertices, i_fromRoute);
+    }
+}
+
 int main(){
     ll N, M;
     cin >> N >> M;
@@ -29,13 +43,37 @@ int main(){
     rep(i, M) {
         cin >> WalkRoutes.at(i).from >> WalkRoutes.at(i).to;
     }
-    vector<VertexStatus> vertices(N+1);
+    vector<VertexStatus> vertices(N+1); // 頂点番号と要素番号を合わせるために１を加算する。
     rep(i, M) {
         auto from_vertex = WalkRoutes.at(i).from;
         auto to_vertex = WalkRoutes.at(i).to;
-        vertices.at(from_vertex).canGo.push_back(to_vertex);
+        vertices.at(to_vertex).fromRoute.push_back(from_vertex);
+        vertices.at(from_vertex).toRoute.push_back(to_vertex);
+    }
+    for (size_t i=1;i<N+1;i++) {
+        vertices.at(i).aliveToRouteNum = vertices.at(i).toRoute.size();
     }
 
-    cout << WalkRoutes.at(2).to << endl;
+    for (size_t i=1;i<N+1;i++) {
+        if (vertices.at(i).toRoute.size() == 0) {
+            if (vertices.at(i).fromRoute.size() == 0) {
+                vertices.at(i).status = -1;
+            } else {
+                vertices.at(i).status = -1;
+                for (ll i_fromRoute: vertices.at(i).fromRoute) {
+                    vertices.at(i_fromRoute).aliveToRouteNum--;
+                    routeBack(vertices, i_fromRoute);
+                }
+            }
+        }
+    }
+
+    ll ans = 0;
+    for (size_t i=1;i<N+1;i++) {
+        if (vertices.at(i).status != -1) {
+            ans++;
+        }
+    }
+    cout << ans << endl;
     return 0;
 }
